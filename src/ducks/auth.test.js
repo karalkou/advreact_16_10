8e4044159cf531d39a2314ea227f485342ab32e1
Yury Karalkou <yury.karalkou@gmail.com>
2017-11-05 16:58:52 +0300

    [YK] HT2.2 Add auth.test.js

---
+++ src/ducks/auth.test.js
@@ -0,0 +1,79 @@
+import firebase from 'firebase'
+import {
+    signUpSaga, signInSaga,
+    signUp, signIn,
+    SIGN_UP_START, SIGN_UP_SUCCESS, SIGN_UP_ERROR,
+    SIGN_IN_START, SIGN_IN_ERROR
+} from './auth'
+import {all, take, call, put} from 'redux-saga/effects'
+
+
+it('should sign up user', () => {
+    const auth = firebase.auth()
+    const gen = signUpSaga()
+
+    expect(gen.next().value).toEqual(take(SIGN_UP_START))
+
+    const authData = {
+        email: '123@123.ru',
+        password: 'qwerty1234'
+    }
+
+    const requestAction = {
+        type: SIGN_UP_START,
+        payload: authData
+    }
+
+    expect(gen.next(requestAction).value).toEqual(call(
+        [auth, auth.createUserWithEmailAndPassword],
+        authData.email, authData.password
+    ))
+
+    const user = {
+        email: authData.email,
+        uid: Date.now().toString()
+    }
+
+    expect(gen.next(user).value).toEqual(put({
+        type: SIGN_UP_SUCCESS,
+        payload: {user}
+    }))
+
+    const error = new Error
+
+    expect(gen.throw(error).value).toEqual(put({
+        type: SIGN_UP_ERROR,
+        payload: {error}
+    }))
+
+})
+
+it('should sign in user', () => {
+    const auth = firebase.auth()
+    const gen = signInSaga()
+
+    expect(gen.next().value).toEqual(take(SIGN_IN_START))
+
+    const authData = {
+        email: '123@123.ru',
+        password: 'qwerty1234'
+    }
+
+    const requestAction = {
+        type: SIGN_IN_START,
+        payload: authData
+    }
+
+    expect(gen.next(requestAction).value).toEqual(call(
+        [auth, auth.signInWithEmailAndPassword],
+        authData.email, authData.password
+    ))
+
+    const error = new Error
+
+    expect(gen.throw(error).value).toEqual(put({
+        type: SIGN_IN_ERROR,
+        payload: {error}
+    }))
+
+})
